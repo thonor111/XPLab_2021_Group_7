baefc5714c15057131e7e175ca7c7ec70663ca76
H-27 <47536612+H-27@users.noreply.github.com>
2021-08-28 19:45:07 +0200

    comments

--- analyses/02_main/analysis_script.R
+++ analyses/02_main/analysis_script.R
@@ -9,35 +9,44 @@ library(shinystan)
 fulldata <- read.csv("clean_data.csv")
 data <- read.csv("short_data.csv")
 
+# plot the means in a bar plot
 source("functions/produce_mean_and_count_bar_plot.R")
 barplot <- produce_mean_and_count_bar_plot(fulldata, bar_width_means=0.5, bar_width_response=0.3)
 ggsave(file="barplot.png", plot=barplot, width=190, height = 110, units="mm")
+
+# plot the chosen issues and their rating
 source("functions/issue_plot.R")
 issueplot <- issue_plot(data)
 ggsave(file="issueplot.png",plot = issueplot, width=300, height = 110, units="mm")
 
+# data as list for stan models
+data_list <- as.list(c(fulldata, N = dim(fulldata)[1]))
 
+# fit the first model, the alternative model
 fit_brms_one <- rstan::stan(
   # where is the Stan code
   file = 'model_one.stan',
   # data to supply to the Stan program
-  data = data,
-  # how many iterations of MCMC
-  iter = 20000,
-  # how many warmup steps
-  warmup = 2000
-)
-fit_brms_two <- rstan::stan(
-  # where is the Stan code
-  file = 'model_two.stan',
-  # data to supply to the Stan program
-  data = data,
+  data = data_list,
   # how many iterations of MCMC
   iter = 20000,
   # how many warmup steps
   warmup = 2000
 )
 
+# fit the second model, the self-categorization model
+fit_brms_two <- rstan::stan(
+  # where is the Stan code
+  file = 'model_two.stan',
+  # data to supply to the Stan program
+  data = data_list,
+  # how many iterations of MCMC
+  iter = 20000,
+  # how many warmup steps
+  warmup = 2000
+)
+
+# calculate marginal likelihood and from that compute the bayes factor
 marg_modelone <- bridgesampling::bridge_sampler(marg_modelone, silent = T)
 marg_modeltwo <- bridgesampling::bridge_sampler(marg_modeltwo, silent = T)
 bridgesampling::bf(marg_modelone, marg_modeltwo)
