bc09f9199f2d8c2ffa8c0794a1accd5e5dfcc38f
H-27 <47536612+H-27@users.noreply.github.com>
2021-08-30 21:05:30 +0200

    update

--- analyses/02_main/analysis_script.R
+++ analyses/02_main/analysis_script.R
@@ -5,8 +5,10 @@ library(rstan)
 library(bridgesampling)
 library(shinystan)
 
-#import clean data
+# import clean data
 fulldata <- read.csv("clean_data.csv")
+# give summary of data (optional)
+#write.csv(summary(fulldata),"summary_data.csv")
 
 # plot the means in a bar plot
 source("functions/produce_mean_and_count_bar_plot.R")
@@ -18,31 +20,44 @@ source("functions/issue_plot.R")
 issueplot <- issue_plot(fulldata)
 ggsave(file="issueplot.png",plot = issueplot, width=500, height = 110, units="mm")
 
+# run an additional frequentist analysis with cumulative link model
+ordinal <- clm(as.factor(response)~ingroupNorm*bothShown, data=fulldata)
+summary(ordinal)
+
+
+
+# fit the data, then Compare the alternative model with the chosen SCT model (can be given for fit_brms_two in file) 
+# using bayes factor on the marginal likelihood 
+  
 # data as list for stan models
 data_list <- as.list(c(fulldata, N = dim(fulldata)[1]))
 
 # fit the first model, the alternative model
 fit_brms_one <- rstan::stan(
   # where is the Stan code
-  file = 'herding.stan',
+  file = "stan_models/herding.stan",
   # data to supply to the Stan program
   data = data_list,
   # how many iterations of MCMC
-  iter = 20000,
+  iter = 10000,
   # how many warmup steps
-  warmup = 2000
+  chains=4,
+  seed=123,
+  control=list(adapt_delta = 0.99)
 )
 
 # fit the second model, the self-categorization model
 fit_brms_two <- rstan::stan(
   # where is the Stan code
-  file = 'SCT.stan',
+  file = "stan_models/SCT.stan",
   # data to supply to the Stan program
   data = data_list,
   # how many iterations of MCMC
-  iter = 20000,
+  iter = 10000,
   # how many warmup steps
-  warmup = 2000
+  chains=4,
+  seed=123,
+  control=list(adapt_delta = 0.99)
 )
 
 # calculate marginal likelihood and from that compute the bayes factor
